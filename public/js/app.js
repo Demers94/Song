359e167ef37574f29468cdd15050ddc7658a441a
Demers94 <demerscharlesantoine@gmail.com>
2016-09-06 19:15:16 -0400

    Improved the notification, added more comments and docblock

--- public/js/app.js
+++ public/js/app.js
@@ -7,10 +7,17 @@ $(function(){
 		var $title = $('#title');
 		var $artist = $('#artist');
 
+		// Get the inputs values.
 		var url = $url.val();
 		var title = $title.val();
 		var artist = $artist.val();
 
+		// Make sure that we have something to send to the converter.
+		if(!url.length || !title.length){
+			return false;
+		}
+
+		// Clear the inputs for the next song.
 		$url.val('');
 		$title.val('');
 		$artist.val('');
@@ -24,17 +31,26 @@ $(function(){
 		return false;
 	});
 
+	// A download has finished, show the notification.
 	socket.on('download finished', function(data){
 		notify('The song "' + data.title + '" has finished downloading');
 	});
 
+	/**
+	 * Create a Web Notification to tell the user the download is complete.
+	 * @param  {string} message 
+	 * @return {void}         
+	 */
 	function notify(message){
 		Notification.requestPermission().then(function(result) {
 			if(!result == 'granted'){
 				return;
 			}
 
-			var notification = new Notification(message);
+			var notification = new Notification('Download complete', {
+				body: message,
+				icon: 'http://www.freeiconspng.com/uploads/accept-tick-icon-12.png',
+			});
 		});
 	}
 });
